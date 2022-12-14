16d03fa78bbce80c7adaef7443cd21eeaa948849
Asep <asepsuherman389@gmail.com>
2022-12-15 02:06:44 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://gallery-app-server.vercel.app/photos" // TODO: replace this
+const studentName = "Asep Suherman" // TODO: replace this
+const studentId = "FE2664682" // TODO: replace this
+
+export { url, studentName, studentId }
